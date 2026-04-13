1c8ab6cf43ab8c6a2151edd93aede8212a1b608e
Ziyad Ahmad <ziyadahmad0371@gmail.com>
2026-04-13 11:35:43 +0530

    Added booking feature

--- src/main/java/com/YoutubeTools/Controller/PageController.java
+++ src/main/java/com/YoutubeTools/Controller/PageController.java
@@ -10,7 +10,6 @@ public class PageController {
     public String home() {
         return "home";
     }
-
     @GetMapping("/Video-details")
     public String videoDetails() {
         return "Video-details";
